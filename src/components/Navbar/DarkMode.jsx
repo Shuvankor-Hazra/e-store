f5fe0ea637ec5d0b9e1fc39b499e937ee59aa1a2
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-10-17 18:24:09 +0600

    Full Navbar added

---
+++ src/components/Navbar/DarkMode.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import DarkMoodBtn from "../../assets/dark-mode-button.png";
+import LightMoodBtn from "../../assets/light-mode-button.png";
+
+const DarkMode = () => {
+    const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
+
+    const element = document.documentElement; // Access to html element
+    console.log(element);
+
+    useEffect(() => {
+        localStorage.setItem("theme", theme);
+        if (theme === "dark") {
+            element.classList.add("dark");
+        } else {
+            element.classList.remove("dark");
+        }
+    }, [element.classList, theme]);
+
+    return (
+        <div className="relative">
+            <img
+                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                src={LightMoodBtn} alt=""
+                className={`w-12 cursor-pointer absolute transition-all duration-300 ${theme === "dark" ? "opacity-0" : "opacity-100"}`} />
+            <img
+                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                src={DarkMoodBtn} alt=""
+                className={`w-12 cursor-pointer`} />
+        </div>
+    )
+}
+
+export default DarkMode
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
+import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
 import { IoMdSearch } from "react-icons/io";
-import { FaCartShopping } from "react-icons/fa6";
+import DarkMode from "./DarkMode";
 
 const MenuLinks = [
     {
@@ -20,6 +21,21 @@ const MenuLinks = [
     },
 ]
 
+const DropdownLinks = [
+    {
+        name: "Trending Products",
+        link: "/#"
+    },
+    {
+        name: "Best Selling",
+        link: "/#"
+    },
+    {
+        name: "Top Rated",
+        link: "/#"
+    },
+]
+
 const Navbar = () => {
     return (
         <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
@@ -32,17 +48,39 @@ const Navbar = () => {
                         {/* Menu Icons */}
                         <div className="hidden lg:block">
                             <ul className="flex items-center gap-4">
-                                {
-                                    MenuLinks.map((data, idx) => (
-                                        <li key={idx}>
-                                            <a
-                                                href={data.link}
-                                                className="inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200"
-                                            >{data.name}
-                                            </a>
-                                        </li>
-                                    ))
-                                }
+                                {MenuLinks.map((data, idx) => (
+                                    <li key={idx}>
+                                        <a
+                                            href={data.link}
+                                            className="inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200"
+                                        >{data.name}
+                                        </a>
+                                    </li>
+                                ))}
+                                {/* Dropdown */}
+                                <li className="relative cursor-pointer group">
+                                    <a href="#"
+                                        className="flex items-center gap-1 font-semibold text-gray-500 dark:hover:text-white py-2"
+                                    >Quick Links
+                                        <span>
+                                            <FaCaretDown className="group-hover:rotate-180 duration-300" />
+                                        </span>
+                                    </a>
+                                    {/* Dropdown Links */}
+                                    <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white shadow-md dark:bg-gray-900 p-3 dark:text-white">
+                                        <ul className="space-y-2">
+                                            {DropdownLinks.map((data, idx) => (
+                                                <li key={idx}>
+                                                    <a
+                                                        href={data.link}
+                                                        className="text-gray-500 dark:hover:text-white duration-200 p-2 hover:bg-primary/20 inline-block font-semibold rounded-md w-full">
+                                                        {data.name}
+                                                    </a>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -54,21 +92,20 @@ const Navbar = () => {
                                 className="search-bar"
                             />
                             <IoMdSearch
-                                className="text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200"
-                            />
+                                className="text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200" />
                         </div>
-
                         {/* Order-Button Section */}
                         <button className="relative p-3">
                             <FaCartShopping
-                                className="text-xl text-gray-600 dark:text-gray-400"
-                            />
+                                className="text-xl text-gray-600 dark:text-gray-400" />
                             <div className="w-4 h-4 bg-red-500 text-white rounded-full absolute flex items-center justify-center text-xs top-0 right-0">
                                 4
                             </div>
                         </button>
                         {/* Dark-Mode Section */}
-                        <div></div>
+                        <div>
+                            <DarkMode />
+                        </div>
                     </div>
                 </div>
             </div>
