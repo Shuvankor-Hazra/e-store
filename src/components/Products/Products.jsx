ab1cd13cd02486093b66d0def964557eccf571a6
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-10-20 09:51:31 +0600

    Products bottom  section added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,6 +4,8 @@ import Img1 from "../../assets/Products/p-1.png"
 import Img2 from "../../assets/Products/p-2.png"
 import Img3 from "../../assets/Products/p-3.png"
 import Img4 from "../../assets/Products/p-4.png"
+import Img5 from "../../assets/Products/p-5.png"
+import Img6 from "../../assets/Products/p-6.png"
 
 const ProductsData = [
     {
@@ -35,6 +37,36 @@ const ProductsData = [
         aosDelay: "600",
     },
 ]
+const ProductsData2 = [
+    {
+        id: 1,
+        img: Img6,
+        title: "Boat Headphone",
+        price: "120",
+        aosDelay: "0",
+    },
+    {
+        id: 2,
+        img: Img5,
+        title: "Rocky Mountain",
+        price: "220",
+        aosDelay: "200",
+    },
+    {
+        id: 3,
+        img: Img4,
+        title: "Goggles",
+        price: "320",
+        aosDelay: "400",
+    },
+    {
+        id: 4,
+        img: Img3,
+        title: "Printed",
+        price: "420",
+        aosDelay: "600",
+    },
+]
 
 const Products = () => {
     return (
@@ -44,6 +76,7 @@ const Products = () => {
                 <Heading title={"Our Products"} subTitle={"Explore Our Products"} />
                 {/* Body Section */}
                 <ProductCard data={ProductsData} />
+                <ProductCard data={ProductsData2} />
             </div>
         </div>
     )
